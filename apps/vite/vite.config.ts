fc5b20e02dd312e8a0d398f554788b1b1f2a6a4c
evgenius1424 <evgenius1424@yandex.ru>
2025-04-21 23:07:35 +0200

    chore: update module aliases and paths to use @repo prefix

--- apps/vite/vite.config.ts
+++ apps/vite/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@ui": path.resolve(__dirname, "../../packages/ui/src"),
+      "@repo/ui": path.resolve(__dirname, "../../packages/ui/src"),
     },
   },
 });
